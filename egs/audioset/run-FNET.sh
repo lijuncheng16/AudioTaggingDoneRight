51d37debadf2366c7918a6c2106f3d9b328817ba
Billy Li <lijuncheng16@gmail.com>
2022-03-08 13:38:59 -0500

    fnet commit

---
+++ egs/audioset/run-FNET.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+export TORCH_HOME=../../pretrained_models
+slurm_id=6974414
+python prep_AudioSet.py --slurm-id ${slurm_id}
+slurm_folder=/local/slurm-${slurm_id}/local/audio/data/datafiles
+model=ast
+dataset=audioset
+# full or balanced for audioset
+set=full
+imagenetpretrain=True
+if [ $set == balanced ]
+then
+  bal=none
+  lr=5e-5
+  epoch=25
+  tr_data=${slurm_folder}/audioset_bal_train_data.json
+else
+  bal=bal
+  lr=1e-5
+  epoch=5
+  tr_data=${slurm_folder}/audioset_bal_unbal_train_data.json
+fi
+te_data=${slurm_folder}/audioset_eval_data.json
+freqm=48
+timem=192
+mixup=0.5
+# corresponding to overlap of 6 for 16*16 patches
+fstride=10
+tstride=10
+batch_size=6
+exp_dir=./exp/audioset-${set}-f$fstride-t$tstride-p$imagenetpretrain-b$batch_size-lr${lr}-fnet_debug
+if [ -d $exp_dir ]; then
+  echo 'exp exist'
+  exit
+fi
+mkdir -p $exp_dir
+echo ${model}
+CUDA_CACHE_DISABLE=1 python -W ignore ../../src/run.py --model ${model} --dataset ${dataset} \
+--data-train ${tr_data} --data-val ${te_data} --exp-dir $exp_dir \
+--label-csv ./data/class_labels_indices.csv --n_class 527 \
+--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model True \
+--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+--tstride $tstride --fstride $fstride --imagenet_pretrain $imagenetpretrain
--- src/run.py
+++ src/run.py
@@ -55,40 +55,42 @@ parser.add_argument('--audioset_pretrain', help='if use ImageNet and audioset pr
 
 args = parser.parse_args()
 
+
+print('now train a audio spectrogram transformer model')
+# dataset spectrogram mean and std, used to normalize the input
+norm_stats = {'audioset':[-4.2677393, 4.5689974], 'esc50':[-6.6268077, 5.358466], 'speechcommands':[-6.845978, 5.5654526]}
+target_length = {'audioset':1024, 'esc50':512, 'speechcommands':128}
+# if add noise for data augmentation, only use for speech commands
+noise = {'audioset': False, 'esc50': False, 'speechcommands':True}
+
+audio_conf = {'num_mel_bins': 128, 'target_length': target_length[args.dataset], 'freqm': args.freqm, 'timem': args.timem, 'mixup': args.mixup, 'dataset': args.dataset, 'mode':'train', 'mean':norm_stats[args.dataset][0], 'std':norm_stats[args.dataset][1],
+              'noise':noise[args.dataset]}
+val_audio_conf = {'num_mel_bins': 128, 'target_length': target_length[args.dataset], 'freqm': 0, 'timem': 0, 'mixup': 0, 'dataset': args.dataset, 'mode':'evaluation', 'mean':norm_stats[args.dataset][0], 'std':norm_stats[args.dataset][1], 'noise':False}
+
+if args.bal == 'bal':
+    print('balanced sampler is being used')
+    samples_weight = np.loadtxt(args.data_train[:-5]+'_weight.csv', delimiter=',')
+    sampler = WeightedRandomSampler(samples_weight, len(samples_weight), replacement=True)
+
+    train_loader = torch.utils.data.DataLoader(
+        dataloader.AudiosetDataset(args.data_train, label_csv=args.label_csv, audio_conf=audio_conf),
+        batch_size=args.batch_size, sampler=sampler, num_workers=args.num_workers, pin_memory=True)
+else:
+    print('balanced sampler is not used')
+    train_loader = torch.utils.data.DataLoader(
+        dataloader.AudiosetDataset(args.data_train, label_csv=args.label_csv, audio_conf=audio_conf),
+        batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
+
+val_loader = torch.utils.data.DataLoader(
+    dataloader.AudiosetDataset(args.data_val, label_csv=args.label_csv, audio_conf=val_audio_conf),
+    batch_size=args.batch_size*2, shuffle=False, num_workers=args.num_workers, pin_memory=True)
 # transformer based model
 if args.model == 'ast':
-    print('now train a audio spectrogram transformer model')
-    # dataset spectrogram mean and std, used to normalize the input
-    norm_stats = {'audioset':[-4.2677393, 4.5689974], 'esc50':[-6.6268077, 5.358466], 'speechcommands':[-6.845978, 5.5654526]}
-    target_length = {'audioset':1024, 'esc50':512, 'speechcommands':128}
-    # if add noise for data augmentation, only use for speech commands
-    noise = {'audioset': False, 'esc50': False, 'speechcommands':True}
-
-    audio_conf = {'num_mel_bins': 128, 'target_length': target_length[args.dataset], 'freqm': args.freqm, 'timem': args.timem, 'mixup': args.mixup, 'dataset': args.dataset, 'mode':'train', 'mean':norm_stats[args.dataset][0], 'std':norm_stats[args.dataset][1],
-                  'noise':noise[args.dataset]}
-    val_audio_conf = {'num_mel_bins': 128, 'target_length': target_length[args.dataset], 'freqm': 0, 'timem': 0, 'mixup': 0, 'dataset': args.dataset, 'mode':'evaluation', 'mean':norm_stats[args.dataset][0], 'std':norm_stats[args.dataset][1], 'noise':False}
-
-    if args.bal == 'bal':
-        print('balanced sampler is being used')
-        samples_weight = np.loadtxt(args.data_train[:-5]+'_weight.csv', delimiter=',')
-        sampler = WeightedRandomSampler(samples_weight, len(samples_weight), replacement=True)
-
-        train_loader = torch.utils.data.DataLoader(
-            dataloader.AudiosetDataset(args.data_train, label_csv=args.label_csv, audio_conf=audio_conf),
-            batch_size=args.batch_size, sampler=sampler, num_workers=args.num_workers, pin_memory=True)
-    else:
-        print('balanced sampler is not used')
-        train_loader = torch.utils.data.DataLoader(
-            dataloader.AudiosetDataset(args.data_train, label_csv=args.label_csv, audio_conf=audio_conf),
-            batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
-
-    val_loader = torch.utils.data.DataLoader(
-        dataloader.AudiosetDataset(args.data_val, label_csv=args.label_csv, audio_conf=val_audio_conf),
-        batch_size=args.batch_size*2, shuffle=False, num_workers=args.num_workers, pin_memory=True)
-
     audio_model = models.ASTModel(label_dim=args.n_class, fstride=args.fstride, tstride=args.tstride, input_fdim=128,
                                   input_tdim=target_length[args.dataset], imagenet_pretrain=args.imagenet_pretrain,
                                   audioset_pretrain=args.audioset_pretrain, model_size='base384')
+elif args.model == 'fnet':
+    audio_model = models.get_fnet()
 
 print("\nCreating experiment directory: %s" % args.exp_dir)
 os.makedirs("%s/models" % args.exp_dir)
